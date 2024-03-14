975b372c09a0a45a13caffe707f2570c2eaa1f81
dgmnkk <125593353+dgmnkk@users.noreply.github.com>
2024-03-14 18:45:47 +0200

    Add base of program

--- task.asm
+++ task.asm
@@ -2,9 +2,46 @@
 .stack 100h
 
 .data
+numbersBuffer dw 10000 dup(?) ; буфер для зберігання чисел
+numbersCount dw 0               ; кількість чисел
 
 .code
-main PROC
-    
-main ENDP
+main:
+    mov ax, @data
+    mov ds, ax
+
+   
+    call readInput
+    call parseNumbers
+    call sort
+    call calculateAverage
+    call calculateMedian
+    call writeOutput
+
+    mov ah, 4Ch
+    int 21h
+
+readInput:
+    ; читання рядка з stdin та зберігання чисел у масив
+    ret
+
+parseNumbers:
+    ; розділення рядка на окремі числа та збереження їх у масив
+    ret
+
+sort:
+    ; сортування масиву чисел
+    ret
+
+calculateAverage:
+    ; обчислення середнього значення
+    ret
+
+calculateMedian:
+    ; обчислення медіани
+    ret
+
+writeOutput:
+    ; виведення середнього значення та медіани
+    ret
 end main
